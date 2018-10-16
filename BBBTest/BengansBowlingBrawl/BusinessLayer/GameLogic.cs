09dcba9726293fd54400476aecc640e7b994023a
Ahmad <Ahmad.a.ali@hotmail.com>
2018-10-16 14:27:13 +0200

    La till designpatterns idee

---
+++ BBBTest/BengansBowlingBrawl/BusinessLayer/GameLogic.cs
@@ -0,0 +1,20 @@
+﻿using BengansBowlingBrawl.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BengansBowlingBrawl.BusinessLayer
+{
+    class GameLogic
+    {
+        public void newOpponentCheck()
+        {
+            var playerOne = new Spelare();
+            var playerTwo = new Spelare();
+
+            var match = new Match(playerOne,playerTwo);
+        }
+
+        public void ruleCheck() { }
+    }
+}
---
+++ BBBTest/BengansBowlingBrawl/Factory/GameHandler.cs
@@ -0,0 +1,15 @@
+﻿using BengansBowlingBrawl.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BengansBowlingBrawl.Factory
+{
+    class GameHandler
+    {
+        public void createGame(Spelare sp1, Spelare sp2)
+        {
+            var match = new Match(sp1, sp2);
+        }
+    }
+}
